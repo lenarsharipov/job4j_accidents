09886f204301f368473121cae8e8cb99cce1f98e
lenarsharipov <lenarsharipov@gmail.com>
2023-05-19 14:41:24 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -2,13 +2,15 @@ package ru.job4j.accidents.controller;
 
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @ThreadSafe
 @Controller
 public class IndexController {
     @GetMapping({"/", "/index"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
 }
