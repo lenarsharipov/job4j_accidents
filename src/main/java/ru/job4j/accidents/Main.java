6f24b6b87a6cccc2461339d4cb098541de130b9a
lenarsharipov <lenarsharipov@gmail.com>
2023-05-19 14:31:06 +0300

    0. Spring MVC [#6877]

--- src/main/java/ru/job4j/accidents/Main.java
+++ src/main/java/ru/job4j/accidents/Main.java
@@ -1,7 +1,11 @@
 package ru.job4j.accidents;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        SpringApplication.run(Main.class, args);
     }
 }
