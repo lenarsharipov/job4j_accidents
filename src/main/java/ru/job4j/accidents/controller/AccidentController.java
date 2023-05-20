713da78d2554f16fe26aa9e76b7acb0c260c0d5b
lenarsharipov <lenarsharipov@gmail.com>
2023-05-20 11:18:18 +0300

    3.1. @RequestParam. Форма редактирования [#308708]

--- src/main/java/ru/job4j/accidents/controller/AccidentController.java
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -3,13 +3,13 @@ package ru.job4j.accidents.controller;
 import lombok.AllArgsConstructor;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.service.AccidentService;
-import org.springframework.ui.Model;
 
 @ThreadSafe
 @Controller
@@ -18,75 +18,58 @@ public class AccidentController {
 
     private final AccidentService accidentService;
     private static final String MESSAGE = "message";
-    private static final String UNABLE_TO_EDIT = "UNABLE TO EDIT SPECIFIED ACCIDENT";
+    private static final String USER_VALUE = "Lenar Sharipov";
+    private static final String UNABLE_TO_UPDATE = "UNABLE TO UPDATE SPECIFIED ACCIDENT";
+    private static final String UNABLE_TO_FIND_BY_ID = "UNABLE TO FIND ACCIDENT BY SPECIFIED ID";
     private static final String ERROR_404_PAGE = "404";
-    private static final String EDIT_PAGE = "edit";
-    private static final String ACCIDENTS_PAGE = "accidents";
+    private static final String REDIRECT_INDEX_PAGE = "redirect:/index";
     private static final String CREATE_ACCIDENT_PAGE = "createAccident";
-    private static final String REDIRECT_ACCIDENTS_PAGE = "redirect:/accidents";
-    private static final String ACCIDENTS_ATTRIBUTE = "accidents";
+    private static final String UPDATE_ACCIDENT_PAGE = "updateAccident";
     private static final String ACCIDENT_ATTRIBUTE = "accident";
-
-    /**
-     * List all persisted accidents.
-     * @param model model.
-     * @return accidents page.
-     */
-    @GetMapping("/accidents")
-    public String showAll(Model model) {
-        model.addAttribute(ACCIDENTS_ATTRIBUTE, accidentService.findAll());
-        return ACCIDENTS_PAGE;
-    }
+    private static final String USER_ATTRIBUTE = "user";
 
     /**
      * Get Accident creation page.
      * @return createAccident page.
      */
     @GetMapping("/createAccident")
-    public String viewCreateAccident() {
+    public String viewCreateAccident(Model model) {
+        model.addAttribute(USER_ATTRIBUTE, USER_VALUE);
         return CREATE_ACCIDENT_PAGE;
     }
 
     /**
      * Save new accident.
-     * @param accident Accident
-     * @return 404 or redirect:/accidents
+     * @param accident Accident.
+     * @return 404 or redirect:/accidents.
      */
     @PostMapping("/saveAccident")
     public String save(@ModelAttribute Accident accident) {
         accidentService.create(accident);
-        return REDIRECT_ACCIDENTS_PAGE;
+        return REDIRECT_INDEX_PAGE;
     }
 
-    /**
-     * Get accident edit page by id.
-     * @param id id.
-     * @param model Model.
-     * @return 404 or /edit page.
-     */
-    @GetMapping("/edit/{id}")
-    public String viewUpdateAccident(@PathVariable int id, Model model) {
+    @GetMapping("/formUpdateAccident")
+    public String viewUpdate(@RequestParam("id") int id, Model model) {
         var accidentOptional = accidentService.findById(id);
         if (accidentOptional.isEmpty()) {
-            model.addAttribute(MESSAGE, UNABLE_TO_EDIT);
+            model.addAttribute(USER_ATTRIBUTE, USER_VALUE);
+            model.addAttribute(MESSAGE, UNABLE_TO_FIND_BY_ID);
             return ERROR_404_PAGE;
         }
         model.addAttribute(ACCIDENT_ATTRIBUTE, accidentOptional.get());
-        return EDIT_PAGE;
+        model.addAttribute(USER_ATTRIBUTE, USER_VALUE);
+        return UPDATE_ACCIDENT_PAGE;
     }
 
-    /**
-     * Update found by id accident.
-     * @param accident Accident.
-     * @return 404 or redirect:/accidents.
-     */
-    @PostMapping("/edit/{id}")
-    public String update(@ModelAttribute Accident accident) {
-        var isUpdated = accidentService.update(accident);
-        if (!isUpdated) {
+    @PostMapping("/updateAccident")
+    public String update(Model model, @ModelAttribute Accident accident) {
+        if (!accidentService.update(accident)) {
+            model.addAttribute(USER_ATTRIBUTE, USER_VALUE);
+            model.addAttribute(MESSAGE, UNABLE_TO_UPDATE);
             return ERROR_404_PAGE;
         }
-        return REDIRECT_ACCIDENTS_PAGE;
+        return REDIRECT_INDEX_PAGE;
     }
 
 }
--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -5,14 +5,23 @@ import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.accidents.service.AccidentService;
 
 @ThreadSafe
 @Controller
 @AllArgsConstructor
 public class IndexController {
 
+    private final AccidentService accidentService;
+    private static final String USER_VALUE = "Lenar Sharipov";
+    private static final String USER_ATTRIBUTE = "user";
+    private static final String ACCIDENTS_ATTRIBUTE = "accidents";
+    private static final String INDEX_PAGE = "index";
+
     @GetMapping({"/", "/index"})
     public String getIndex(Model model) {
-        return "index";
+        model.addAttribute(USER_VALUE, USER_ATTRIBUTE);
+        model.addAttribute(ACCIDENTS_ATTRIBUTE, accidentService.findAll());
+        return INDEX_PAGE;
     }
 }
